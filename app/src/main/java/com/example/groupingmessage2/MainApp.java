2111dfb6cea8df4f9f2125fe66ed673e183d0df7
Leftddr <qhddnjs75@gmail.com>
2021-11-03 14:30:42 +0900

    need to bug fix

--- app/src/main/java/com/example/groupingmessage2/MainApp.java
+++ app/src/main/java/com/example/groupingmessage2/MainApp.java
@@ -14,17 +14,20 @@ import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
 import io.reactivex.functions.Consumer;
 import sdk.chat.app.firebase.ChatSDKFirebase;
+import sdk.chat.core.dao.Thread;
 import sdk.chat.core.dao.Message;
 import sdk.chat.core.dao.User;
 import sdk.chat.core.hook.Hook;
 import sdk.chat.core.hook.HookEvent;
 import sdk.chat.core.interfaces.ThreadType;
 import sdk.chat.core.session.ChatSDK;
+import sdk.chat.core.types.AccountDetails;
 import sdk.chat.firebase.adapter.module.FirebaseModule;
 import sdk.chat.firebase.push.FirebasePushModule;
 import sdk.chat.firebase.ui.FirebaseUIModule;
@@ -32,30 +35,39 @@ import sdk.chat.firebase.upload.FirebaseUploadModule;
 import sdk.chat.ui.module.UIModule;
 import io.reactivex.disposables.Disposable;
 import sdk.guru.common.DisposableMap;
-
+//RXJava를 이용한 코드구나...
 public class MainApp extends Application {
     protected DisposableMap dm = new DisposableMap();
+    protected int max_num = 20, min_num = 5;
+    protected Random random = new Random();
     @Override
     public void onCreate(){
         super.onCreate();
         try {
             quickStart(this, "pre_3", "adadad", false);
             ArrayList<User> users = new ArrayList<>();
-            User currentUser = ChatSDK.core().currentUser();
-            currentUser.setName("Test");
-            dm.add(ChatSDK.thread().createThread(users).subscribe(thread -> {
-                dm.add(ChatSDK.thread().sendMessageWithText("Hello", thread).subscribe(() -> {
 
-                }));
+            dm.add(ChatSDK.thread().createThread("Name", users, ThreadType.PrivateGroup, "custom-id", "http://image-url").subscribe(thread -> {
+                Log.e("mytag", "success");
+                for(int i = 0 ; i < 100 ; i++) {
+                    int rand = random.nextInt(max_num - min_num + 1) + min_num;
+                    try {
+                        java.lang.Thread.sleep(rand * 1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    sendTextMessage("hihihi", thread);
+                }
+            }, e -> {
+                Log.e("mytag", e.toString());
             }));
+            listenForReceivedMessage();
         } catch (Exception e) {
-            Log.i("1", "\n\n\n\n\n");
             e.printStackTrace();
-            Log.i("1", "\n\n\n\n\n");
+            Log.e("mytag", e.toString());
         }
-        //ChatSDK.ui().startSplashScreenActivity(this);
     }
-    /*
+
     public void sendTextMessage (String message, Thread thread) {
         Disposable d = ChatSDK.thread().sendMessageWithText(message, thread).subscribe(() -> {
             // Handle Success
@@ -63,17 +75,12 @@ public class MainApp extends Application {
             // Handle failure
         });
     }
-    */
+
     public void listenForReceivedMessage () {
         // Synchronous code
+        // 여기에 broadcast Receiver 필요
         ChatSDK.hook().addHook(Hook.sync(data -> {
             Message message = (Message) data.get(HookEvent.Message);
         }), HookEvent.MessageReceived);
-
-        // Asynchronous code
-        ChatSDK.hook().addHook(Hook.async(data -> Completable.create(emitter -> {
-            // ... Async code here
-            emitter.onComplete();
-        })), HookEvent.MessageReceived);
     }
 }
